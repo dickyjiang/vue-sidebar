9dd60e3ebcfda5a3ea054776e30c96a4c56dced3
Dicky Jiang <dickyjuwono@gmail.com>
2021-09-23 16:48:01 +0700

    done add tw

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@fortawesome/fontawesome-free/js/all'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@fortawesome/fontawesome-free/js/all";
+import "./index.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
