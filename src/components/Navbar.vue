beade12f168a96f0c9a04a616e74784fcad2674d
Dicky Jiang <dickyjuwono@gmail.com>
2021-10-07 11:01:10 +0700

    add .env file

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="  bg-gray-200 py-4 px-3 ">
+  <div class="  bg-gray-50 py-4 px-3 ">
       <div class="flex justify-between">
         <p>logo</p>
-        <div class="flex">
-            <p>link 1</p>
+        <div class="flex gap-3 items-center">
+            <div class="text-white text-sm bg-black py-1 px-2 rounded-full">
+                Get in Touch
+            </div>
             <p>link 2</p>
             <p>link 3</p>
         </div>
